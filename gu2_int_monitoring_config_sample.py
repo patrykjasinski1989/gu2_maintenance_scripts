3349fb2d4751195782da0cc59106653616fb4054
patrykjasinski1989 <patrykjasinski1989@gmail.com>
2020-06-02 19:32:04 +0200

    Add GU2 INT monitoring

---
+++ gu2_int_monitoring_config_sample.py
@@ -0,0 +1,27 @@
+zabbix_ptk_url = 'http://172.30.69.240/zabbix/dashboard.php'
+zabbix_ptk_username = ''
+zabbix_ptk_password = ''
+zabbix_ptk_styles = [
+    'http://172.30.69.240/zabbix/css.css',
+    'http://172.30.69.240/zabbix/styles/default.css',
+    'http://172.30.69.240/zabbix/styles/color.css',
+    'http://172.30.69.240/zabbix/styles/icon.css',
+    'http://172.30.69.240/zabbix/styles/blocks.css',
+    'http://172.30.69.240/zabbix/styles/screen.css',
+    'http://172.30.69.240/zabbix/styles/themes/darkorange/main.css',
+]
+
+zabbix_eai_tp_url = 'http://126.193.3.114/zabbix/dashboard.php'
+
+zabbix_om_tp_url = 'https://126.201.7.199/zabbix/index.php'
+zabbix_om_tp_username = ''
+zabbix_om_tp_password = ''
+
+zabbix_ov_tp_url = 'https://126.185.85.230/zabbix-2.4.3/index.php'
+zabbix_ov_tp_username = ''
+zabbix_ov_tp_password = ''
+
+zabbix_new_url = 'https://126.185.85.228/zabbix/index.php'
+zabbix_new_username = ''
+zabbix_new_password = ''
+zabbix_new_style = 'https://126.185.85.228/zabbix/assets/10am2h0/styles/dark-theme.css'
